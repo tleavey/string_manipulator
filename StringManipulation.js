936fb0b374a37c7deeaad7da69fbb60d6cc975d9
tleavey <timothy.leavey@gmail.com>
2018-10-24 11:18:53 -0700

    Added main.js file that imports the class.

--- StringManipulation.js
+++ StringManipulation.js
@@ -50,3 +50,5 @@ class StringManipulation {
       : "No repeated words";
   }
 }
+
+module.exports = StringManipulation;
---
+++ main.js
@@ -0,0 +1,5 @@
+const StringManipulation = require('./StringManipulation.js');
+
+console.log(StringManipulation.repeatedWords('Example of using the StringManipulation class because an example can be helpful.'));
+
+console.log(StringManipulation.removePunctuation('Example of using the StringManipulation class because an example can be helpful.'));
